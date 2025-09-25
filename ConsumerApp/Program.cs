0e1513dc6f33da3f379c18ba54ce48062aea4a01
Craig Mcloughlin <craig.mcloughlin@oneadvanced.com>
2025-09-25 21:49:57 +0100

    build

---
+++ ConsumerApp/Program.cs
@@ -0,0 +1,10 @@
+﻿
+var person = new Person { FirstName = "Albert", LastName = "Einstein ", YearOfBirth = 1879 };
+string json = Newtonsoft.Json.JsonConvert.SerializeObject(person);
+Console.WriteLine(json);
+
+public class Person {
+   public string FirstName { get; init; }
+   public string LastName { get; init; }
+   public int YearOfBirth { get; init; }
+}
